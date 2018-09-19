67c99655c00eb701d360d14d759ae2842b099390
Dani Gunawan <danigunawan.elektroug@gmail.com>
2018-09-19 19:37:16 +0700

    first with si golang

---
+++ 2.values.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("GO" + "Lang")
+    fmt.Println("1+1", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
+        // $ go run values.go
+        // golang
+        // 1+1 = 2
+        // 7.0/3.0 = 2.3333333333333335         
+        // false
+        // true
+        // false
+
+
+
+
+Language Version:  1.10.2
