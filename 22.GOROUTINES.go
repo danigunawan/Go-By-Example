8b1c7ed8c03bdf28816b3e74bdff126166487f03
Dani Gunawan <danigunawan.elektroug@gmail.com>
2018-12-08 10:05:17 +0700

    sudah ok

---
+++ 22.GOROUTINES.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+func main() {
+	f("direct")
+	go f("goroutine")
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+	fmt.Scanln()
+	fmt.Println("done")
+}
+
+// $ go run goroutines.go
+// direct : 0
+// direct : 1
+// direct : 2
+// goroutine : 0
+// going
+// goroutine : 1
+// goroutine : 2
+// <enter>
+// done
